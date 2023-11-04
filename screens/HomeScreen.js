708fb9557e8471bb6e748ea0f8d8cbf3cfce45c6
Arjun K <arjunk9135@gmail.com>
2023-11-04 22:08:23 +0530

    screen 1 commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Animated, Pressable, StyleSheet, Text, View } from 'react-native'
 import React, { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
+import {Ionicons} from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
 const HomeScreen = () => {
+  const navigation=useNavigation();
+  const moveAnimation=new Animation();
     useLayoutEffect(()=>{
-     const navigation=useNavigation();
-     navigation.setOptions
+      navigation.setOptions(
+      {
+        headerLeft:()=><Text>Hello Arjun </Text>,
+        headerStyles:{
+          backgroundColor:"#F5F5F5",
+          shadowColor:"transparent",
+          shadowOpacity:0.3,
+          shadowOffset:{width:-1,height:1},
+          shadowRadius:3
+        },
+        headerRight:()=><Pressable style={{flexDirection:"row",alignItems:"center",gap:10}}>
+        <Ionicons name="notifications-outline" size={24} color="black" />
+        <Ionicons name="ios-location-outline" size={24} color="black" />
+            <Pressable>
+              <Animated.Text style={[styles.text,{transform: [{ translateX: moveAnimation }] },
+              ]}>
+                <Text>Kochi</Text>
+              </Animated.Text>
+            </Pressable>
+        </Pressable>
+      }
+     )
     },[])
   return (
     <View>
